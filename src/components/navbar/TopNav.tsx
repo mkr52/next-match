ce86f0033d0b29ac25d2516479611db00af4dce8
mkr52 <161114463+mkr52@users.noreply.github.com>
2024-06-01 13:23:49 +0530

    End of section 2

---
+++ src/components/navbar/TopNav.tsx
@@ -0,0 +1,40 @@
+import { Button, Link, Navbar, NavbarBrand, NavbarContent, NavbarItem } from '@nextui-org/react'
+import React from 'react'
+import { GiMatchTip } from 'react-icons/gi'
+import NavLink from './NavLink'
+
+export default function TopNav() {
+    return (
+        <Navbar
+            maxWidth='xl'
+            className='bg-gradient-to-r from-yellow-500 to-fuchsia-500'
+            classNames={
+                {
+                    item: [
+                        'text-xl',
+                        'text-white',
+                        'uppercase',
+                        'data-[active=true]:text-yellow-200'
+                    ]
+                }
+            }
+        >
+            <NavbarBrand as={Link} href="/" className="flex items-center gap-2">
+                <GiMatchTip size={40} className='text-gray-200' />
+                <div className='text-3xl font-bold flex'>
+                    <span className='text-gray-900'>Next</span>
+                    <span className='text-gray-200'>Match</span>
+                </div>
+            </NavbarBrand>
+            <NavbarContent justify="center">
+                <NavLink href="/members" label='Matches' />
+                <NavLink href="/lists" label='Lists' />
+                <NavLink href="/messages" label='Messages' />
+            </NavbarContent>
+            <NavbarContent justify="end">
+                <Button as={Link} href="/login" variant='bordered' className="text-white">Login</Button>
+                <Button as={Link} href="/register" variant='bordered' className="text-white">Register</Button>
+            </NavbarContent>
+        </Navbar>
+    )
+}
